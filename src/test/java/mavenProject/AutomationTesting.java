2bb95caeb722b719e66fc9cb465386ebe52e980d
Mosarrof Hossain <phmosharaf@gmail.com>
2018-07-29 17:52:53 -0400

    It newly done

--- src/test/java/mavenProject/AutomationTesting.java
+++ src/test/java/mavenProject/AutomationTesting.java
@@ -1,10 +1,21 @@
 package mavenProject;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class AutomationTesting {
 
 	public static void main(String[] args) {
 		
- System.out.println("Successfully done ");
+	
+		System.setProperty("webdriver.driver.chrome", "C:/Users/Mosarrof Hossain/Utilities/chromedriver.exe");
+		
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.google.com");
+		
+		
 	}
+	
 
 }
