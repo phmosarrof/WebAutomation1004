5edbbb5ad14bc6e2f7865346f3c5ca36cfa29169
Mosarrof Hossain <phmosharaf@gmail.com>
2018-07-29 19:11:28 -0400

    Code added

--- src/test/java/mavenProject/AutomationTesting.java
+++ src/test/java/mavenProject/AutomationTesting.java
@@ -1,11 +1,12 @@
 package mavenProject;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AutomationTesting {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Throwable {
 		
 	
 		System.setProperty("webdriver.driver.chrome", "C:/Users/Mosarrof Hossain/Utilities/chromedriver.exe");
@@ -13,8 +14,14 @@ public class AutomationTesting {
 		
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://www.google.com");
+		driver.manage().window().maximize();
 		System.out.println(driver.getTitle());
+		driver.findElement(By.xpath("//*[@id=\"hptl\"]/a[1]")).click();
+		driver.navigate().back();
+		driver.findElement(By.xpath("//*[@id=\"hptl\"]/a[2]")).click();
+		Thread.sleep(5000);
 		driver.close();
+	
 		
 		
 		
