c9399c5333098fc7b187fa9733635577a3e7b582
Mosarrof Hossain <phmosharaf@gmail.com>
2018-07-30 09:20:00 -0400

    Footer links tested

---
+++ src/test/java/abc_practices/GoogleFooterLinks.java
@@ -0,0 +1,61 @@
+package abc_practices;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+
+public class GoogleFooterLinks {
+
+	public static void main(String[] args) throws InterruptedException {
+
+
+		System.setProperty("webdriver.driver.chrome", "C:/Users/Mosarrof Hossain/Utilities/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.google.com/");
+		driver.manage().window().maximize();
+		Thread.sleep(2000);
+
+		driver.findElement(By.linkText("Privacy")).click();
+		if(driver.getPageSource().contains("Privacy"))
+			System.out.println(" The footer link test 01 Passed");
+		else
+			System.out.println("The footer link test 01 failed");
+
+		Assert.assertEquals(true, driver.getPageSource().contains("Privacy"));
+		Assert.assertEquals(4, 2+2);
+		Thread.sleep(2000);
+		driver.navigate().back();
+		
+		driver.findElement(By.linkText("Terms")).click();
+		if(driver.getPageSource().contains("Terms"))
+			System.out.println(" The footer link test 02 Passed");
+		else
+			System.out.println("The footer link test 02 failed");
+	
+		Assert.assertEquals(true, driver.getPageSource().contains("Terms"));
+		Thread.sleep(2000);
+		driver.navigate().back();		
+
+		driver.findElement(By.linkText("Settings")).click();
+		if(driver.getPageSource().contains("Settings"))
+			System.out.println(" The footer link test 03 Passed");
+		else
+			System.out.println("The footer link test 03 failed");
+
+		Assert.assertEquals(true, driver.getPageSource().contains("Settings"));
+
+		Thread.sleep(2000);
+
+		driver.close();
+
+
+
+
+	}
+
+
+
+
+}
--- src/test/java/mavenProject/AutomationTesting.java
+++ src/test/java/mavenProject/AutomationTesting.java
@@ -3,29 +3,42 @@ package mavenProject;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+//import org.junit.Assert;
+
+
+
 
 public class AutomationTesting {
 
-	public static void main(String[] args) throws Throwable {
-		
-	
-		System.setProperty("webdriver.driver.chrome", "C:/Users/Mosarrof Hossain/Utilities/chromedriver.exe");
-		
-		
+	public static void main(String[] args) throws InterruptedException {
+
+		System.setProperty("webdriver.driver.chrome","C:/Users/Mosarrof Hossain/Utilities/chromedriver.exe");
+
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://www.google.com");
 		driver.manage().window().maximize();
 		System.out.println(driver.getTitle());
 		driver.findElement(By.xpath("//*[@id=\"hptl\"]/a[1]")).click();
 		driver.navigate().back();
+		Thread.sleep(2000);
 		driver.findElement(By.xpath("//*[@id=\"hptl\"]/a[2]")).click();
-		Thread.sleep(5000);
-		driver.close();
-	
+		Thread.sleep(2000);
+		driver.findElement(By.linkText("Privacy")).click();
+		if(driver.getPageSource().contains("Privacy"))
+			System.out.println(" The footer link test Passed");
+		else
+			System.out.println("The footer link test failed");
+
+		Assert.assertEquals(true, driver.getPageSource().contains("Privacy"));
+		//Assert.assertEquals(4, 2+2);
 		
-		
-		
+		driver.quit();
+
+
+
+
 	}
-	
+
 
 }
